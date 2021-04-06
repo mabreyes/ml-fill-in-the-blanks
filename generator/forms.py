ea64f02d93e481b65640571e09c5cf2d0f568029
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2021-04-06 22:07:59 +0800

    Remove label and add placeholder programmatically

--- generator/forms.py
+++ generator/forms.py
@@ -1,8 +1,11 @@
 from django import forms
 
+
 class GeneratorForm(forms.Form):
-    textarea = forms.CharField(max_length=100)
+    textarea = forms.CharField(max_length=100, widget=forms.TextInput(
+        attrs={"placeholder": "Enter Text Here. Enter ____ (4 underscores) for the word you want to be predicted. E.g. I am a ____ person.",
+               "class": "form-control-lg"}))
 
     def __init__(self, *args, **kwargs):
         super(GeneratorForm, self).__init__(*args, **kwargs)
-        self.fields['textarea'].label = "Enter Text Here"
+        self.fields['textarea'].label = ""
