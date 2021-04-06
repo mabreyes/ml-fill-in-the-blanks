d11b5b554c84d62638d55a20bfbcb79b84b0b0c9
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2021-04-06 21:00:05 +0800

    Implement boostrap styling in form inputs

--- generator/forms.py
+++ generator/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class GeneratorForm(forms.Form):
     textarea = forms.CharField(max_length=100)
+
+    def __init__(self, *args, **kwargs):
+        super(GeneratorForm, self).__init__(*args, **kwargs)
+        self.fields['textarea'].label = "Enter Text Here"
