b95e0b5e556621a9169c7b2c69fe6834e5152ca9
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2021-04-07 20:53:59 +0800

    Add database settings

--- bert/settings.py
+++ bert/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'bert.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['ML_FITB_NAME'],
+        'USER': os.environ['ML_FITB_USER'],
+        'PASSWORD': os.environ['ML_FITB_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
