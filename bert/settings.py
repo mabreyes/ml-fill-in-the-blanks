df06436dbacf7d78f0dd328c4e79b672b4578845
Marc Anthony Reyes <marc.reyes@mynt.xyz>
2021-04-07 23:52:01 +0800

    Use environment variables instead of explicit declaration

--- bert/settings.py
+++ bert/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bs^x%vmb!3uudlocysi1%dpwb#eszdju#15fim(4tkmme2ozum'
+SECRET_KEY = os.environ['ML_FITB_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
